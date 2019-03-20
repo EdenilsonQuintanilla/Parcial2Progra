06847a3ab0a195ccc93ed91454d8e08221887acc
EdenilsonQuintanilla <48040961+EdenilsonQuintanilla@users.noreply.github.com>
2019-03-19 20:00:00 -0600

    Add files via upload

---
+++ parcial2/parcial2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            metodo1 mt1 = new metodo1();
+            mt1.multi();
+
+            metodo2 mt2 = new metodo2();
+            mt2.divi();
+
+            metodo3 mt3 = new metodo3();
+            mt3.nombres();
+
+            metodo4 mt4 = new metodo4();
+            mt4.edad();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ parcial2/parcial2/metodo4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial2
+{
+    class metodo4
+    {
+        public void edad();
+        string edad;
+        int contador = 1;
+
+        while (contador <= 5)
+
+
+    }
+}
